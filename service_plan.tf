8a4066f5d0c26941e3a8a171e8109881e380108f
rockeyrazzz <59052426+rockeyrazzz@users.noreply.github.com>
2025-08-04 17:22:58 +0530

    Create service_plan.tf

---
+++ service_plan.tf
@@ -0,0 +1,8 @@
+# File: service_plan.tf
+resource "azurerm_service_plan" "plan" {
+  name                = var.app_service_plan_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
