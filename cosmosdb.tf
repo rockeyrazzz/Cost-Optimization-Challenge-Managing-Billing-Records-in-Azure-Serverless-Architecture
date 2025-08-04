59aac5567742883bca6f071000e7d3b48b25a8de
rockeyrazzz <59052426+rockeyrazzz@users.noreply.github.com>
2025-08-04 17:24:07 +0530

    Create cosmosdb.tf

---
+++ cosmosdb.tf
@@ -0,0 +1,17 @@
+# File: cosmosdb.tf
+resource "azurerm_cosmosdb_account" "cosmos" {
+  name                = var.cosmos_account_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+}
